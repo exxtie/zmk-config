1a0862763f4f154833390f8edfb331242169f590
Egor Basnak <basnak2014@mail.ru>
2024-04-12 20:30:40 +0300

    TEST17

--- config/ru_keys.h
+++ config/ru_keys.h
@@ -3,8 +3,29 @@
 #define RU_Y E
 #define RU_K R
 #define RU_E T
-#define RU_H Y
+#define RU_N Y
 #define RU_G U
 #define RU_SH I
 #define RU_SHb O
 #define RU_Z P
+
+#define RU_F A
+#define RU_bl S
+#define RU_V D
+#define RU_A F
+#define RU_P G
+#define RU_R H
+#define RU_O J
+#define RU_L K
+#define RU_D L
+#define RU_ZH ;
+
+#define RU_YA Z
+#define RU_CH X
+#define RU_C C
+#define RU_M V
+#define RU_I B
+#define RU_T N
+#define RU_b M
+#define RU_B ,
+#define RU_YU .
